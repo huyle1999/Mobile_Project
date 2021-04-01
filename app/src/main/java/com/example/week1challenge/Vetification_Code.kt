dbdf2d9416b1337d285b4d2d2012ae4e580e304a
unknown <danganhhao835@gmail.com>
2021-04-01 15:49:15 +0700

    add last layout

---
+++ app/src/main/java/com/example/week1challenge/Vetification_Code.kt
@@ -0,0 +1,11 @@
+package com.example.week1challenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Vetification_Code : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vetification__code)
+    }
+}
